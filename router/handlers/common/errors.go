ae399fac487a3308663a1bacde7e18bd4f48203c
root <root@c0d6db2f-068d-4ea5-8b7e-84768c6ea164>
2020-06-05 18:11:10 +0000

    error loging

--- router/handlers/common/errors.go
+++ router/handlers/common/errors.go
@@ -1,5 +1,10 @@
 package common
 
+import (
+	"errors"
+	"net/http"
+)
+
 type AppError struct {
 	Err  error
 	Code int
@@ -7,4 +12,16 @@ type AppError struct {
 
 func (e AppError) Error() string {
 	return e.Err.Error()
+}
+
+func WrapError(err error, code int) *AppError {
+	return &AppError{err, code}
+}
+
+func MakeError(msg string, code int) *AppError {
+	if msg == "" {
+		msg = http.StatusText(code)
+	}
+
+	return WrapError(errors.New(msg), code)
 }
