9921b1c08b24fb1cec5995c2995e826fa28e511b
root <root@c0d6db2f-068d-4ea5-8b7e-84768c6ea164>
2020-06-06 10:47:42 +0000

    menu

--- frontend/src/layout/components/menu/menu.js
+++ frontend/src/layout/components/menu/menu.js
@@ -5,11 +5,20 @@ import './styles/menu.scss';
 export default function () {
    const root = document.querySelector('.menu');
    const $showBtn = root.querySelector('.show-menu-js');
+
+   /** @type {HTMLElement} */
    const $list = root.querySelector('.menu-list-js');
 
+   let expanded = false;
+
+   setExpanded(expanded);
    $showBtn.addEventListener('click', () => {
-      const state = $showBtn.getAttribute('aria-expanded') === 'true';
-      $showBtn.setAttribute('aria-expanded', !state);
-      setClassIf($list, 'menu__list--expanded', !state);
+      setExpanded(!expanded);
    });
+
+   function setExpanded(val) {
+      expanded = val;
+      $showBtn.setAttribute('aria-expanded', expanded);
+      setClassIf($list, 'menu__list--expanded', expanded);
+   }
 }
