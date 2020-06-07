c16104560ef48c6a3f61a485cd437aad5ebb2fa4
Dmitry Gashko <dimagashko@gmail.com>
2020-06-07 14:05:49 +0000

    tags cloud

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,15 +1,16 @@
 
 import base from '@/layout/base';
 
-
+import '@/styles/common/info-aside.scss';
 import '@/styles/common/categories-list.scss';
 import '@/styles/common/category.scss';
+import '@/styles/common/tags-list.scss';
+import '@/styles/common/tag.scss';
 import '@/styles/common/post.scss';
 
 import './styles/popular-post.scss';
 import './styles/popular.scss';
 import './styles/posts-list.scss';
-import './styles/hp-aside.scss';
 import './styles/home.scss';
 
 base();
